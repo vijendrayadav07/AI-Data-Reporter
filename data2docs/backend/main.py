4884cea4ffab168c31b396cf49eaed3056d4dcb6
Vijendra Yadav <vijendraydv35@gmail.com>
2025-08-22 15:25:32 +0530

    ai-data-reports

--- data2docs/backend/main.py
+++ data2docs/backend/main.py
@@ -131,4 +131,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # create tables if not exist
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
+
